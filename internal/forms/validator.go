b7242989c15a3cceb7d7570988ccd415e728abeb
Peter Phanvan <pfunnyyy@gmail.com>
2022-06-28 23:32:17 +0200

    repaired the MinLength func in Validator: text can be equal to the min. length

--- internal/forms/validator.go
+++ internal/forms/validator.go
@@ -33,7 +33,7 @@ func (f *Form) Required(fields ...string) {
 
 func (f *Form) MinLength(field string, length int) bool {
 	s := f.Get(field)
-	if len(s) <= length {
+	if len(s) < length {
 		f.Errors.Add(field, fmt.Sprintf("Minimum length must be at least %d characters.", length))
 		return false
 	}

536af88a17bf4e61e15f3b5d61fb44f324cd50f1
Peter Phanvan <pfunnyyy@gmail.com>
2022-06-28 08:53:08 +0200

    implemented passing data to the templates

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,18 +2,24 @@ package handlers
 
 import (
 	"github.com/phanvanpeter/my-portfolio/internal/render"
+	"github.com/phanvanpeter/my-portfolio/models"
 	"net/http"
 )
 
 // Home renders a Home page
 func Home(w http.ResponseWriter, r *http.Request) {
 	file := "home.page"
-	render.Template(w, file)
+
+	strMap := make(map[string]string)
+	strMap["author"] = "Peter Phanvan"
+	render.Template(w, file, &models.TemplateData{
+		StringMap: strMap,
+	})
 
 }
 
 // About renders an About page
 func About(w http.ResponseWriter, r *http.Request) {
 	file := "about.page"
-	render.Template(w, file)
+	render.Template(w, file, nil)
 }
--- internal/render/render.go
+++ internal/render/render.go
@@ -1,6 +1,7 @@
 package render
 
 import (
+	"github.com/phanvanpeter/my-portfolio/models"
 	"html/template"
 	"log"
 	"net/http"
@@ -10,13 +11,13 @@ var TmplFolder = "./templates/"
 const tmplExtension = ".gohtml"
 
 // Template renders and sends to browser an HTML from the given template file
-func Template(w http.ResponseWriter, file string) {
+func Template(w http.ResponseWriter, file string, td *models.TemplateData) {
 	tmpl, err := template.ParseFiles(TmplFolder + file + tmplExtension)
 	if err != nil {
 		log.Fatalf("Error parsing a file %s; %s", file, err)
 	}
 
-	err = tmpl.Execute(w, nil)
+	err = tmpl.Execute(w, td)
 	if err != nil {
 		log.Fatal("error rendering a template", err)
 	}

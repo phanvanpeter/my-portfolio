cfdd032e1756f64221604aeb9c5e6e81459a4fac
Peter Phanvan <pfunnyyy@gmail.com>
2022-06-28 13:00:59 +0200

    adding comment to DBRepository

--- repository/repository.go
+++ repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import "github.com/phanvanpeter/my-portfolio/models"
 
+// DBRepository is an interface for allowing various database systems to connect
 type DBRepository interface {
 	GetTasks() ([]models.Task, error)
 }

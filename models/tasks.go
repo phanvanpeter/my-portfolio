5125f1f8c64028f269f50ef3ebd2c98518747ee0
Peter Phanvan <pfunnyyy@gmail.com>
2022-06-28 18:04:20 +0200

    implementing postgres operations for tasks: get, add, delete

--- models/tasks.go
+++ models/tasks.go
@@ -1,6 +1,11 @@
 package models
 
+import "time"
+
 type Task struct {
-	ID   int
-	Task string
+	ID        int
+	Task      string
+	Done      bool
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
--- repository/postgres/postgresrepo.go
+++ repository/postgres/postgresrepo.go
@@ -1,10 +1,12 @@
 package postgres
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"fmt"
 	"github.com/phanvanpeter/my-portfolio/models"
+	"time"
 )
 
 func NewRepo(db *sql.DB) *postgresRepo {
@@ -17,43 +19,83 @@ type postgresRepo struct {
 	DB *sql.DB
 }
 
-var fakeTasks = []models.Task{
-	{1, "First task"},
-	{2, "Second task"},
-	{3, "Third task"},
-	{4, "Fourth task"},
-}
-var counter = 5
 
-// GetTasks loads all the tasks saved in the file
+// GetTasks loads all the tasks saved in the database
 func (p *postgresRepo) GetTasks() ([]models.Task, error) {
-	return fakeTasks, nil
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	var tasks []models.Task
+
+	query := `
+		select id, task, done, created_at, updated_at
+		from tasks
+		order by updated_at`
+
+	rows, err := p.DB.QueryContext(ctx, query)
+	if err != nil {
+		return tasks, errors.New(fmt.Sprintf("Unable to fetch the tasks from the database: %s", err))
+	}
+
+	for rows.Next() {
+		var t models.Task
+		err = rows.Scan(
+			&t.ID,
+			&t.Task,
+			&t.Done,
+			&t.CreatedAt,
+			&t.UpdatedAt,
+		)
+		if err != nil {
+			return tasks, errors.New(fmt.Sprintf("Unable to scan the loaded tasks: %s", err))
+		}
+		tasks = append(tasks, t)
+	}
+
+	if err = rows.Err(); err != nil {
+		return tasks, errors.New(fmt.Sprintf("Error scanning the tasks: %s", err))
+	}
+
+	return tasks, nil
 }
 
 // AddTask adds a new task to the database
 func (p *postgresRepo) AddTask(task string) error {
-	t := models.Task{counter, task}
-	counter++
-	fakeTasks = append(fakeTasks, t)
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	query := `
+		insert into tasks 
+		(task, created_at, updated_at) 
+		values ($1, $2, $3)`
+
+	_, err := p.DB.ExecContext(ctx, query,
+		task,
+		time.Now(),
+		time.Now(),
+	)
+
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error inserting the task: %s", err))
+	}
+
 	return nil
 }
 
 // DeleteTask deletes a task with the given id
 func (p *postgresRepo) DeleteTask(id int) error {
-	// finds the index of the task to be deleted
-	var index = -1
-	for i, t := range fakeTasks {
-		if t.ID == id {
-			index = i
-			break
-		}
-	}
-	if index == -1 {
-		return errors.New(fmt.Sprintf("There is no task with the id %d", id))
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
 
-	// deleting the task
-	fakeTasks = append(fakeTasks[:index], fakeTasks[index + 1:]...)
+	query := `
+		delete from tasks 
+		where id = $1`
+
+	_, err := p.DB.ExecContext(ctx, query, id)
+
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error deleting the task with id %d: %s", id, err))
+	}
 
 	return nil
 }
